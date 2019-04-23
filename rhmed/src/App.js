c5ec0a237a91f66cd1d88c17ec1679b490da0c2e
motorcitycode <detropia.com@gmail.com>
2019-04-23 13:11:22 -0400

    initial commit of new-rhmed

--- rhmed/src/App.js
+++ rhmed/src/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
-import Login from './components/Login';
-import { RegisterPage } from './components/Register';
-import { HomePage } from './components/HomePage';
-//import GasPrices from './components/GasPrices';
-import PrivateRoute from './components/PrivateRoute';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/protected">Protected Page</Link>
-          </li>
-        </ul>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={RegisterPage} />
-        <PrivateRoute exact path="/protected" component={HomePage} />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
