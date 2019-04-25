4ba380afa3edb21701e8f2fa600e9c8bee350de5
Andrew Benedict <atbenedict@gmail.com>
2019-04-25 15:38:13 -0400

    trying to get deployed with api issues

--- rhmed/src/components/Login.js
+++ rhmed/src/components/Login.js
@@ -1,77 +1,78 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-const url = process.env.REACT_APP_API_URL;
-
+const url = "https://revo-health.herokuapp.com";
 
 const initialUser = {
-    email: '',
-    password: ''
+  email: "",
+  password: ""
 };
 
 class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: { ...initialUser },
-            message: ''
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: { ...initialUser },
+      message: ""
+    };
+  }
 
-    inputHandler = (event) => {
-        const { name, value } = event.target;
-        this.setState({ user: { ...this.state.user, [name]: value } });
-    }
+  inputHandler = event => {
+    const { name, value } = event.target;
+    this.setState({ user: { ...this.state.user, [name]: value } });
+  };
 
-    submitHandler = (event) => {
-        event.preventDefault();
-        axios.post(`${url}/api/auth/login`, this.state.user)
-            .then((res) => {
-                if (res.status === 200 && res.data) {
-                    console.log("res.data: ", res.data);
-                    console.log("this.state.user: ", this.state.user);
-                    localStorage.setItem('secret_token', res.data.token);
-                    localStorage.setItem('user_id', this.state.user.user_id);
-                    this.props.history.push('/');
-                } else {
-                    throw new Error();
-                }
-            })
-            .catch((err) => {
-                this.setState({
-                    message: 'Authentication failed.',
-                    user: { ...initialUser },
-                });
-            });
-    }
+  submitHandler = event => {
+    event.preventDefault();
+    axios
+      .post(`${url}/api/auth/login`, this.state.user)
+      .then(res => {
+        if (res.status === 200 && res.data) {
+          console.log("res.data: ", res.data);
+          console.log("this.state.user: ", this.state.user);
+          localStorage.setItem("secret_token", res.data.token);
+          localStorage.setItem("user_id", this.state.user.user_id);
+          this.props.history.push("/");
+        } else {
+          throw new Error();
+        }
+      })
+      .catch(err => {
+        this.setState({
+          message: "Authentication failed.",
+          user: { ...initialUser }
+        });
+      });
+  };
 
-    render() {
-        return (
-            <div className="user-form">
-                <form onSubmit={this.submitHandler}>
-                    <label htmlFor="email">Email: </label>
-                    <input
-                        type="text"
-                        // id="email"
-                        name="email"
-                        value={this.state.user.email}
-                        onChange={this.inputHandler}
-                    /><br/>
-                    <label htmlFor="password">Password: </label>
-                    <input
-                        type="password"
-                        // id="password"
-                        name="password"
-                        value={this.state.user.password}
-                        onChange={this.inputHandler}
-                    /><br/>
-                    <button type="submit">Submit</button>
-                </form>
-                {this.state.message ? (<h4>{this.state.message}</h4>) : undefined}
-            </div>
-
-        );
-    }
+  render() {
+    return (
+      <div className="user-form">
+        <form onSubmit={this.submitHandler}>
+          <label htmlFor="email">Email: </label>
+          <input
+            type="text"
+            // id="email"
+            name="email"
+            value={this.state.user.email}
+            onChange={this.inputHandler}
+          />
+          <br />
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            // id="password"
+            name="password"
+            value={this.state.user.password}
+            onChange={this.inputHandler}
+          />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+        {this.state.message ? <h4>{this.state.message}</h4> : undefined}
+      </div>
+    );
+  }
 }
 
 export default Login;
--- rhmed/src/components/Register.js
+++ rhmed/src/components/Register.js
@@ -1,107 +1,106 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-const url = process.env.REACT_APP_API_URL;
-
+const url = "https://revo-health.herokuapp.com";
 
 const initialUser = {
-    email: '',
-    password: '',
-    first_name: '',
-    last_name: ''
-}
+  email: "",
+  password: "",
+  first_name: "",
+  last_name: ""
+};
 
 class Register extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            user: {...initialUser},
-            message: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: { ...initialUser },
+      message: ""
+    };
+  }
+
+  inputHandler = event => {
+    const { name, value } = event.target;
+    this.setState({ user: { ...this.state.user, [name]: value } });
+  };
+
+  submitHandler = event => {
+    event.preventDefault();
+    axios
+      .post(`${url}/api/auth/registration`, this.state.user)
+      .then(res => {
+        if (res.status === 201) {
+          localStorage.setItem("rhmed_email", this.state.user.email);
+          this.setState({
+            message: "Registration successful",
+            user: { ...initialUser }
+          });
+        } else {
+          throw new Error();
         }
-    }
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          message: "Registration failed.",
+          user: { ...initialUser }
+        });
+      });
+  };
 
-    inputHandler = (event) => {
-        const { name, value } = event.target;
-        this.setState({ user: { ...this.state.user, [name]: value } });
-    }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.submitHandler}>
+          <label htmlFor="first_name">First Name: </label>
+          <input
+            type="text"
+            // id="first_name"
+            name="first_name"
+            value={this.state.user.first_name}
+            onChange={this.inputHandler}
+          />
+          <br />
+          <label htmlFor="last_name">Last Name: </label>
+          <input
+            type="last_name"
+            // id="last_name"
+            name="last_name"
+            value={this.state.user.last_name}
+            onChange={this.inputHandler}
+          />
+          <br />
+          <label htmlFor="email">Email: </label>
+          <input
+            type="text"
+            // id="email"
+            name="email"
+            value={this.state.user.email}
+            onChange={this.inputHandler}
+          />
+          <br />
 
+          <label htmlFor="password">Password: </label>
+          <input
+            type="text"
+            // id="password"
+            name="password"
+            value={this.state.user.password}
+            onChange={this.inputHandler}
+          />
+          <br />
+          <input
+            type="hidden"
+            name="user_id"
+            value={Date.now()}
+            onChange={this.inputHandler}
+          />
 
-    submitHandler = (event) => {
-        event.preventDefault();
-        axios.post(`${url}/api/auth/registration`, this.state.user)
-            .then((res) => {
-                if (res.status === 201) {
-                    localStorage.setItem('rhmed_email', this.state.user.email);
-                    this.setState({
-                        message: 'Registration successful',
-                        user: { ...initialUser },
-                    });
-                } else {
-                    throw new Error();
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-                this.setState({
-                    message: 'Registration failed.',
-                    user: { ...initialUser },
-                });
-            });
-    }
-
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.submitHandler}>
-                <label htmlFor="first_name">First Name: </label>
-                    <input
-                        type="text"
-                        // id="first_name"
-                        name="first_name"
-                        value={this.state.user.first_name}
-                        onChange={this.inputHandler}
-                    /><br/>
-                    <label htmlFor="last_name">Last Name: </label>
-                    <input
-                        type="last_name"
-                        // id="last_name"
-                        name="last_name"
-                        value={this.state.user.last_name}
-                        onChange={this.inputHandler}
-                    /><br/>
-                    <label htmlFor="email">Email: </label>
-                    <input
-                        type="text"
-                        // id="email"
-                        name="email"
-                        value={this.state.user.email}
-                        onChange={this.inputHandler}
-                    /><br/>
-
-                    <label htmlFor="password">Password: </label>
-                    <input
-                        type="text"
-                        // id="password"
-                        name="password"
-                        value={this.state.user.password}
-                        onChange={this.inputHandler}
-                    /><br/>
-                    <input 
-                        type="hidden"
-                        name="user_id"
-                        value={Date.now()}
-                        onChange={this.inputHandler}
-                    />
-
-
-                    <button type="submit">Submit</button>
-                </form>
-                {this.state.message ? (<h4>{this.state.message}</h4>) : undefined}
-            </div>
-
-        );
-    }
-
-
+          <button type="submit">Submit</button>
+        </form>
+        {this.state.message ? <h4>{this.state.message}</h4> : undefined}
+      </div>
+    );
+  }
 }
 export default Register;
